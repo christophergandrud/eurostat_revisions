d1b42a68b90fd3701f103131e17ab134e4a12707
christophergandrud <christopher.gandrud@gmail.com>
2016-10-27 14:35:48 +0100

    deficit table update

--- analysis/regression_models_tables.R
+++ analysis/regression_models_tables.R
@@ -162,7 +162,7 @@ debt_no_greece <- debt %>% filter(country != 'Greece')
 
 # Debt Models
 m_no_g1 <- lm(cum_revision ~
-               central_gov_debt + euro_member + excessdef +
+                  central_gov_debt + euro_member + excessdef +
                as.factor(country),
               data = debt_no_greece)
 
@@ -172,52 +172,51 @@ m_no_g2 <- lm(cum_revision ~
               data = debt_no_greece)
 
 m_no_g3 <- lm(cum_revision ~
-               central_gov_debt + euro_member +
-               yrcurnt_corrected + finstress_mean +
+                  central_gov_debt + euro_member +
+                  finstress_mean + yrcurnt_corrected +
                as.factor(country),
               data = debt_no_greece)
 
 m_no_g4 <- lm(cum_revision ~
-               central_gov_debt + euro_member +
-               yrcurnt_corrected * finstress_mean +
+                  central_gov_debt + euro_member *
+                  finstress_mean +
                as.factor(country),
               data = debt_no_greece)
 
 m_no_g5 <- lm(cum_revision ~
-               central_gov_debt + euro_member +
-               endog_3 + finstress_mean +
+                  central_gov_debt + euro_member +
+                  finstress_mean * yrcurnt_corrected +
                as.factor(country),
               data = debt_no_greece)
 
 m_no_g6 <- lm(cum_revision ~
-               central_gov_debt + euro_member +
-               endog_3 * finstress_mean +
+                  central_gov_debt + euro_member +
+                  finstress_mean + endog_3 +
                as.factor(country),
               data = debt_no_greece)
 
 m_no_g7 <- lm(cum_revision ~
-               central_gov_debt + euro_member +
-               fiscal_trans_gfs + gdp_growth +
+                  central_gov_debt + euro_member +
+                  finstress_mean * endog_3 +
                 as.factor(country),
               data = debt_no_greece)
 
 m_no_g8 <- lm(cum_revision ~
-               central_gov_debt + euro_member +
-               yrcurnt_corrected * finstress_mean +
-                fiscal_trans_gfs + gdp_growth +
+                  central_gov_debt + euro_member +
+                  fiscal_trans_gfs + gdp_growth +
                 as.factor(country),
               data = debt_no_greece)
 
 m_no_g9 <- lm(cum_revision ~
-                central_gov_debt + euro_member +
-                contracts +
+                  central_gov_debt + euro_member +
+                  contracts +
                as.factor(country),
               data = debt_no_greece)
 
-m_no_g10 <- lm(cum_revision ~
-                    central_gov_debt * euro_member + excessdef +
-                    endog_3 * finstress_mean +
-                    gdp_growth +
+m_no_g10 <- lm(um_revision ~
+                   central_gov_debt * euro_member + excessdef +
+                   finstress_mean * endog_3 +
+                   gdp_growth +
                     as.factor(country),
            data = debt_no_greece)
 
@@ -228,16 +227,6 @@ m_no_g_garbage_can <- lm(cum_revision ~
                          as.factor(country),
                      data = debt_no_greece)
 
-var_labels_2 <- c('Revised Cent. Gov. Debt', 'Euro Member', 'EDP',
-                  'Election Timing', 'Unscheduled Elect.', 'Scheduled Elect.',
-                  'Financial Stress', 'Fiscal Transparency',
-                  'GDP Growth', 'Contracts',
-                  'Debt * Euro',
-                  'Elect. Timing * Fin. Stress',
-                  'Unscheduled Elect. * Fin. Stress',
-                  'Scheduled Elect. * Fin. Stress')
-
-
 # Create output table
 stargazer(m_no_g1, m_no_g2, m_no_g3, m_no_g4, m_no_g5, m_no_g6, m_no_g7,
           m_no_g8, m_no_g9, m_no_g10,
@@ -247,10 +236,10 @@ stargazer(m_no_g1, m_no_g2, m_no_g3, m_no_g4, m_no_g5, m_no_g6, m_no_g7,
           #add.lines = list(c('Country FE?', rep('Yes', 9))),
           dep.var.labels = 'Cumulative Debt Revisions',
           label = 'results_no_greece',
-          covariate.labels = var_labels_2,
+          covariate.labels = var_labels_1,
           star.cutoffs = c(0.05, 0.01, 0.001),
           title = 'Linear Regression Estimation of Debt Revisions (Excluding Greece)',
-          add.lines = list(c('Country FE?', rep('Yes', 13))),
+          add.lines = list(c('Country FE?', rep('Yes', 10))),
           font.size = 'tiny',
           out = 'working_paper/tables/debt_regressions_no_greece.tex')
 
@@ -308,62 +297,65 @@ m2_2 <- lm(cum_revision ~
            data = deficit)
 
 m2_3 <- lm(cum_revision ~
-               general_gov_deficit * euro_member +
+               general_gov_deficit + euro_member +
+               finstress_mean + yrcurnt_corrected +
                as.factor(country),
            data = deficit)
 
 m2_4 <- lm(cum_revision ~
-               general_gov_deficit + euro_member +
-               yrcurnt_corrected + finstress_mean +
+               general_gov_deficit + euro_member *
+               finstress_mean +
                as.factor(country),
            data = deficit)
 
 m2_5 <- lm(cum_revision ~
                general_gov_deficit +
-               euro_member + endog_3 + finstress_mean +
+               euro_member +
+               finstress_mean * yrcurnt_corrected +
                as.factor(country),
            data = deficit)
 
 m2_6 <- lm(cum_revision ~
                general_gov_deficit + euro_member +
-               endog_3 * finstress_mean +
+               finstress_mean + endog_3 +
                as.factor(country),
            data = deficit)
 
 m2_7 <- lm(cum_revision ~
                general_gov_deficit + euro_member +
-               fiscal_trans_gfs + gdp_growth +
+               finstress_mean * endog_3 +
                 as.factor(country),
            data = deficit)
 
 m2_8 <- lm(cum_revision ~
                general_gov_deficit + euro_member +
-               yrcurnt_corrected * finstress_mean +
-                fiscal_trans_gfs + gdp_growth +
+               fiscal_trans_gfs + gdp_growth +
                 as.factor(country),
            data = deficit)
 
 
 m2_9 <- lm(cum_revision ~
                 general_gov_deficit + euro_member +
-                contracts +
+               contracts +
                as.factor(country),
                data = deficit)
 
 m2_10 <- lm(cum_revision ~
                 general_gov_deficit * euro_member + excessdef +
-                endog_3 * finstress_mean +
+                finstress_mean * endog_3 +
                 gdp_growth +
                 as.factor(country),
                data = deficit)
 
 # Create output table
-var_labels_1 <- c('Revised Gen. Gov. Deficit',
+var_labels_deficit <- c('Revised Gen. Gov. Deficit',
                   'Euro Member', 'EDP',
+                  'Financial Stress',
                   'Election Timing', 'Unscheduled Elect.', 'Scheduled Elect.',
-                  'Financial Stress', 'Fiscal Transparency',
+                  'Fiscal Transparency',
                   'GDP Growth', 'Contracts',
                   'Debt * Euro',
+                  'Euro * Fin. Stress',
                   'Elect. Timing * Fin. Stress',
                   'Unscheduled Elect. * Fin. Stress',
                   'Scheduled Elect. * Fin. Stress')
@@ -375,10 +367,10 @@ stargazer(m2_1, m2_2, m2_3, m2_4, m2_5, m2_6, m2_7, m2_8,
           omit.stat = c('f', 'ser'), # so that it fits on the page
           out.header = F,
           dep.var.labels = 'Cumulative Deficit Revisions',
-          covariate.labels = var_labels_1,
+          covariate.labels = var_labels_deficit,
           star.cutoffs = c(0.05, 0.01, 0.001),
           label = 'deficit_results',
           title = 'Linear Regression Estimation of Deficit Revisions (Full Sample)',
-          add.lines = list(c('Country FE?', rep('Yes', 12))),
+          add.lines = list(c('Country FE?', rep('Yes', 10))),
           font.size = 'tiny',
           out = 'working_paper/tables/deficit_regressions.tex')
